e2bc92e82730fc5b2e080ffd09c1fb524219928d
Pilathraj <pilathraj@gmail.com>
2018-04-26 11:20:52 +0530

    task.bat updated

--- appveyor/task.bat
+++ appveyor/task.bat
@@ -4,7 +4,7 @@ call phpize 2>&1
 
 call configure --help
 
-call configure --disable-all --enable-session --enable-debug --enable-cli --enable-cgi --enable-runkit --enable-runkit-spl_object_id --enable-json  2>&1
+call configure --enable-debug --enable-cli --enable-cgi --enable-runkit 2>&1
     
 nmake /nologo 2>&1
 
