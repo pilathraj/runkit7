8fbbb7a356394cf291c2dd3ed0eca9a1f4a04f0b
Pilathraj <pilathraj@gmail.com>
2018-04-26 11:14:07 +0530

    configure help added

--- appveyor/task.bat
+++ appveyor/task.bat
@@ -2,7 +2,9 @@
 
 call phpize 2>&1
 
-call configure --disable-all --enable-session --enable-debug --enable-cli --enable-cgi --enable-runkit --enable-runkit-spl_object_id --enable-json  --enable-debug-pack 2>&1
+call configure --help
+
+call configure --disable-all --enable-session --enable-debug --enable-cli --enable-cgi --enable-runkit --enable-runkit-spl_object_id --enable-json  2>&1
     
 nmake /nologo 2>&1
 
