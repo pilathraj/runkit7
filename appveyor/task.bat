61b9b8d3fb086a88f03fdbe2d1f5dc663aee0659
Pilathraj <pilathraj@gmail.com>
2018-05-04 15:36:14 +0530

    update task

--- appveyor/task.bat
+++ appveyor/task.bat
@@ -4,7 +4,7 @@ call phpize 2>&1
 
 call configure --help
 
-call configure --disable-all [--disable-zts] --enable-runkit=share
+call configure --disable-all --disable-zts --enable-runkit=yes
     
 nmake /nologo 2>&1
 
