ba4cbe673b879857be1e2c5e82bae2fdc6b57960
Pilathraj <pilathraj@gmail.com>
2018-05-04 18:38:12 +0530

    task modified 123

--- appveyor/task.bat
+++ appveyor/task.bat
@@ -4,7 +4,7 @@ call phpize 2>&1
 
 call configure --help
 
-call configure --disable-all --enable-debug --enable-runkit-spl_object_id
+call configure --disable-all --enable-debug --enable-runkit --enable-runkit-spl_object_id
     
 nmake /nologo 2>&1
 
